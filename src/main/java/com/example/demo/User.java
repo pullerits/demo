fd19b36bc6bde88d8b21ec2f00f4f0cd4c79639e
pullerits <ralf.pullerits@gmail.com>
2025-03-25 23:30:39 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+public class User {
+    private Long id;
+    private String username;
+    private String password; // In production, use encrypted passwords!
+
+    // Constructor
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and setters
+    public Long getId() {
+      return id;
+    }
+    
+    public void setId(Long id) {
+      this.id = id;
+    }
+    
+    public String getUsername() {
+      return username;
+    }
+    
+    public void setUsername(String username) {
+      this.username = username;
+    }
+    
+    public String getPassword() {
+      return password;
+    }
+    
+    public void setPassword(String password) {
+      this.password = password;
+    }
+}
